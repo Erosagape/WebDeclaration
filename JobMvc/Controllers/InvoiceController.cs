ea335b5e9893c47caa5572a6c2134b1d46fe4cdb
Erosagape <Bo@t1234>
2017-03-25 14:21:12 +0700

    commit

---
+++ JobMvc/Controllers/InvoiceController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace JobMvc.Controllers
+{
+    public class InvoiceController : Controller
+    {
+        // GET: Invoice
+        public ActionResult Invoice()
+        {
+            return View();
+        }
+        public ActionResult getDecInvoice_Header()
+        {
+            var data = new DecInvoice_Header().get();
+            return Json(data, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
